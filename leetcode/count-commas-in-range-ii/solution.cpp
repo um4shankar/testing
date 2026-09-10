4df58d2d37da29a144843f7e045f0e939f2ec692
um4shankar <141544732+um4shankar@users.noreply.github.com>
2026-09-10 15:09:06 +0530

    Update file via viaSocket

--- leetcode/count-commas-in-range-ii/solution.cpp
+++ leetcode/count-commas-in-range-ii/solution.cpp
@@ -1,1 +1,12 @@
-undefined
+class Solution {
+public:
+    long long countCommas(long long n) {
+        long long ans = 0;
+        long long x=1000;
+        while(n >= x){
+            ans += (n-x+1);
+            x *= 1000;
+        }
+        return ans;
+    }
+};
